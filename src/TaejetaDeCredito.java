70faa3866dbbe007701c3b17e254a76f3be29a39
Carlos M. Ramos <145763670+CARLRAM2482@users.noreply.github.com>
2024-10-16 20:36:57 -0500

    v1.1

---
+++ src/TaejetaDeCredito.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class TaejetaDeCredito {
+    private double limite;
+    private double saldo;
+    private List<Compra> listaDeCompra;
+
+    public TaejetaDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.listaDeCompra = listaDeCompra;
+    }
+}
